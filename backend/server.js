5739c612bf372a576f36a9c9fc1aa28f10dfc05e
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-09-26 21:16:30 +0530

    Model added and post request modified

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
 //backend
 import express from "express";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 const app = express();
 
-app.post("/products", (req, res) => {
-  res.send("Server is ready!!!");
+app.use(express.json()); //middleware that allows us to accept json data in the req.body
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body; //user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.listen(5000, () => {
